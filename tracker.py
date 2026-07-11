a7c0f108f65d4a1e3a6b2cd1840ad63256f7fd1e
jcgartley <jocelyn.gartley@gmail.com>
2026-07-11 16:15:05 -0400

    added looping menu for dynamic input

--- tracker.py
+++ tracker.py
@@ -7,8 +7,26 @@ def print_applications():
     for app in applications:
         print(f"{app['company']} - {app['role']} - {app['status']}")
 
-add_application("Google", "Software Engineer", "Applied")
-add_application("Microsoft", "SWE Intern", "Applied")
+while(True):
+    print("\n1. Add Application")
+    print("2. View Applications")
+    print("3. Quit")
+    choice = input("Choose an Option: ")
+
+    if choice == '1':
+        company = input("Company: ")
+        role = input("Role: ")
+        status = input("Status: ")
+        add_application(company, role, status)
+    elif choice == '2':
+        print_applications()
+    elif choice == '3':
+        break
+    else:
+        print("Invalid Choice, Try Again")
+
+#add_application("Google", "Software Engineer", "Applied")
+#add_application("Microsoft", "SWE Intern", "Applied")
 
 print_applications()
 
