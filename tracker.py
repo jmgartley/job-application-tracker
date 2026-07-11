afaebbb4d46d1c40c66194496f20221badd8f235
jcgartley <jocelyn.gartley@gmail.com>
2026-07-11 16:05:42 -0400

    added function to print items from applications

--- tracker.py
+++ tracker.py
@@ -3,7 +3,12 @@ applications = []
 def add_application(company, role, status) -> None:
     applications.append({"company": company, "role": role, "status": status})
 
+def print_applications():
+    for app in applications:
+        print(f"{app['company']} - {app['role']} - {app['status']}")
+
 add_application("Google", "Software Engineer", "Applied")
 add_application("Microsoft", "SWE Intern", "Applied")
-print(applications)
+
+print_applications()
 
