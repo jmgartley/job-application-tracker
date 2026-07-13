cbb184cfb19189b16b436d2594a9cba1f468ead8
jcgartley <jocelyn.gartley@gmail.com>
2026-07-13 17:24:28 -0400

    added confirmation to deleting applications, also now awaits user input before returning to menu

--- tracker.py
+++ tracker.py
@@ -25,13 +25,22 @@ def update_status(index, status):
     else:
         print("Invalid Application Number")
 
+def delete_application(index):
+    if 0 <= index < len(applications):
+        removed = applications.pop(index)
+        print(f"Removed {removed['company']} - {removed['role']}")
+        save_applications()
+    else:
+        print("Invalid Application Number")
+
 applications = load_applications()
 
 while(True):
     print("\n1. Add Application")
     print("2. View Applications")
     print("3. Update Status")
-    print("4. Quit")
+    print("4. Delete Application")
+    print("5. Quit")
     choice = input("Choose an Option: ")
 
     if choice == '1':
@@ -48,12 +57,21 @@ while(True):
         new_status = input("New Status: ")
         update_status(int(num) - 1, new_status)
     elif choice == '4':
+        print_applications()
+        num = input("Which application would you like to delete? ")
+        confirm = input(f"Are you sure you want to delete #{num}? (y/n): ")
+        if confirm.lower() == "y":
+            delete_application(int(num) - 1)
+        else:
+            print("Action Cancelled.")
+    elif choice == '5':
         break
     else:
         print("Invalid Choice, Try Again")
 
+    input("Press Enter to Continue...")
 #add_application("Google", "Software Engineer", "Applied")
 #add_application("Microsoft", "SWE Intern", "Applied")
 
-print_applications()
+#print_applications()
 
