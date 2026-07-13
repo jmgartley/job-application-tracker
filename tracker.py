4f8499eb3087fcd0f0e83c9df25d45959444ddc9
jcgartley <jocelyn.gartley@gmail.com>
2026-07-13 17:12:04 -0400

    added option to update status of application

--- tracker.py
+++ tracker.py
@@ -4,8 +4,8 @@ def add_application(company, role, status) -> None:
     applications.append({"company": company, "role": role, "status": status})
 
 def print_applications():
-    for app in applications:
-        print(f"{app['company']} - {app['role']} - {app['status']}")
+    for index, app in enumerate(applications):
+        print(f"{index+1}. {app['company']} - {app['role']} - {app['status']}")
 
 def save_applications():
     with open("applications.json", "w") as file:
@@ -18,12 +18,20 @@ def load_applications():
     except FileNotFoundError:
         return []
 
+def update_status(index, status):
+    if 0 <= index < len(applications):
+        applications[index]["status"] = status
+        save_applications()
+    else:
+        print("Invalid Application Number")
+
 applications = load_applications()
 
 while(True):
     print("\n1. Add Application")
     print("2. View Applications")
-    print("3. Quit")
+    print("3. Update Status")
+    print("4. Quit")
     choice = input("Choose an Option: ")
 
     if choice == '1':
@@ -35,6 +43,11 @@ while(True):
     elif choice == '2':
         print_applications()
     elif choice == '3':
+        print_applications()
+        num = input("Which application would you like to update? ")
+        new_status = input("New Status: ")
+        update_status(int(num) - 1, new_status)
+    elif choice == '4':
         break
     else:
         print("Invalid Choice, Try Again")
